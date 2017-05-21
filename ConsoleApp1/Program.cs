5ed62ce58731d78114c0f2b3016a755aa35585a1
pompedus <pompedus@gmail.com>
2017-05-21 17:26:29 +0200

    Program.cs-edits
    Added some gameplay

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,37 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Hello there handsome! \n Choose an option: \n 1. Start!\n 2.Exit :'(");
+                    string answer = Console.ReadLine();
+                    if ((Convert.ToInt32(answer) == 1))
+                    {
+                        Console.WriteLine("Lets go!");
+                        break;
+                    }
+                    if ((Convert.ToInt32(answer) == 2))
+                    {
+                        Console.WriteLine("Ohh... Good bye D:");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Answer! Try again!");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid Answer! Try again!");
+                }
+                catch(ArgumentNullException)
+                {
+                    Console.WriteLine("Invalid Answer! Try again!");
+                }
+            }
+            
         }
     }
 }
